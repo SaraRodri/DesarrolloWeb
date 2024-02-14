7a0a9dd76341c45b312e34c8ae005a31efe8b396
SaraRodri <91100211+SaraRodri@users.noreply.github.com>
2024-02-13 19:05:13 -0500

    Update CariSellaso.kt

--- CariSellaso.kt
+++ CariSellaso.kt
@@ -10,7 +10,7 @@ fun main(){
         val carisellaso = 0..1
         var sorpresa:Int = carisellaso.random()
         println("Escoja, cara o sello:")
-        var jugador = readLine()!!.toString()
+        var jugador = readLine()!!.toString().lowercase()
         var jugador_res:Int = 3
         when (jugador){
             "cara"-> jugador_res = 0
